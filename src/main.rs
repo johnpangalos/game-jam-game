55b9dd91d5ea49d4dd962afce7e915f715fbd8e5
Jacob Torrång <jacob.torrang@skf.com>
2022-02-26 18:39:07 +0100

    Add window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
 
 trait Card {
     fn strength(&self) -> i32;
@@ -18,6 +21,9 @@ impl Card for Basic {
 }
 
 #[derive(Component)]
+struct FpsText;
+
+#[derive(Component)]
 struct Player;
 
 #[derive(Component)]
@@ -35,7 +41,10 @@ struct Board([Option<Basic>; 3]);
 #[derive(Component)]
 struct Hand([Option<Basic>; 10]);
 
-fn add_cards(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
+
     commands
         .spawn()
         .insert(Player)
@@ -50,6 +59,37 @@ fn add_cards(mut commands: Commands) {
         .insert(Name("Computer".to_string()))
         .insert(Hitpoints(10))
         .insert(Board([Option::None; 3]));
+
+    commands
+        .spawn_bundle(TextBundle {
+            style: Style {
+                align_self: AlignSelf::FlexEnd,
+                ..Default::default()
+            },
+            text: Text {
+                sections: vec![
+                    TextSection {
+                        value: "FPS: ".to_string(),
+                        style: TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 60.0,
+                            color: Color::WHITE,
+                        },
+                    },
+                    TextSection {
+                        value: "".to_string(),
+                        style: TextStyle {
+                            font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                            font_size: 60.0,
+                            color: Color::GOLD,
+                        },
+                    },
+                ],
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(FpsText);
 }
 
 fn run_cards(
@@ -78,9 +118,22 @@ fn display_health(query: Query<(&Name, &Hitpoints)>) {
     }
 }
 
+fn text_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
+    for mut text in query.iter_mut() {
+        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(average) = fps.average() {
+                text.sections[1].value = format!("{:.2}", average);
+            }
+        }
+    }
+}
+
 fn main() {
     App::new()
-        .add_startup_system(add_cards)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_startup_system(setup)
+        .add_system(text_update_system)
         .add_system(run_cards.label("run_cards"))
         .add_system(display_health.after("run_cards"))
         .run();
